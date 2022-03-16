180ef6d7c96fd7bb400e78c83169777146154c78
macktsawyer <macktsawyer@yahoo.com>
2022-03-15 20:12:00 -0400

    Fetching images

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,13 +2,12 @@ const asyncHandler = require('express-async-handler');
 const InventoryModel = require('../models/inventoryModel');
 const { cloudinary } = require('../utils/cloudinary');
 
-
 const newInventory = asyncHandler(async (req, res) => {
     try {
         const fileStr = req.body.data;
         const uploadedResponse = await cloudinary.uploader.
         upload(fileStr, {
-            upload_preset: 'inv_lib_dump'
+            folder: 'inv_lib_dump'
         })
         console.log(uploadedResponse)
         res.json({msg: 'Image upload successful'})
@@ -18,6 +17,18 @@ const newInventory = asyncHandler(async (req, res) => {
     }
 });
 
+const getInventory = asyncHandler(async (req, res) => {
+    const { resources } = await cloudinary.search
+        .expression('folder:inv_lib_dump')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
+    const publicIds = resources.map(file => file.public_id);
+
+    res.send(publicIds);
+})
+
 module.exports = {
     newInventory,
+    getInventory
 }
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { newInventory } = require('../controllers/inventoryController');
+const { newInventory, getInventory } = require('../controllers/inventoryController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/newInventory', newInventory)
 
+router.get('/getInventory', getInventory)
 
 module.exports = router
