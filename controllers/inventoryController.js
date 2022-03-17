04243548a55990ee5eb1ee24fb421d00c17150fc
macktsawyer <macktsawyer@yahoo.com>
2022-03-16 21:05:09 -0400

    Working on databse and image server linking

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,16 +1,40 @@
 const asyncHandler = require('express-async-handler');
 const InventoryModel = require('../models/inventoryModel');
 const { cloudinary } = require('../utils/cloudinary');
+const uuid = require('uuid');
 
 const newInventory = asyncHandler(async (req, res) => {
     try {
-        const fileStr = req.body.data;
+        const file_id = uuid.v4();
+        const { item_name, item_desc, item_price } = req.body;
+        const fileStr = req.body.image_data;
+        if(!item_name || !item_desc || !item_price) {
+            res.status(400)
+            throw new Error('Please add all fields')
+        }
         const uploadedResponse = await cloudinary.uploader.
         upload(fileStr, {
-            folder: 'inv_lib_dump'
+            folder: 'inv_lib_dump',
+            public_id: file_id,
         })
+        const itemInfo = InventoryModel.create({
+            id: file_id,
+            item: item_name,
+            description: item_desc,
+            price: item_price,
+            image: fileStr
+        })
+        if (itemInfo) {
+            res.status(201).json({
+                id: itemInfo.id,
+                item: itemInfo.item,
+                msg: 'Upload successful'
+            })
+        } else {
+            res.status(400)
+            throw new Error('Item not submitted, error occured')
+        }
         console.log(uploadedResponse)
-        res.json({msg: 'Image upload successful'})
     } catch (error) {
         console.error(error)
         res.json(500).json({err: 'Upload didnt work out well'})
@@ -23,7 +47,8 @@ const getInventory = asyncHandler(async (req, res) => {
         .sort_by('public_id', 'desc')
         .max_results(30)
         .execute();
-    const publicIds = resources.map(file => file.public_id);
+    const publicIds = resources.map(file => file.public_id.split('/')[1]);
+
 
     res.send(publicIds);
 })
--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Inventory = new mongoose.Schema({
+    id: { type: String, require: true},
     item: { type: String, required: true },
     description: { type: String, required: false},
     price: { type: String, required: true }, 
